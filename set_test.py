d4c2848e2f3021ea0eb6241687b4e54251719ccd
찬익 박 <piaocanyi1997@gmail.com>
2023-04-22 01:00:30 +0900

    heapq,set modify

---
+++ set_test.py
@@ -0,0 +1,17 @@
+s = set()
+s.add(10)
+s.add(10)
+s.add(50)
+s.add(20)
+s.add(50)
+print(s)
+
+# pop(): 랜덤 으로 지워짐
+# s.pop()
+
+# clear(): 요소 전부 삭제
+# s.clear()
+
+# remove(): 특정값 삭제
+s.remove(20)
+print(s)
