9dd4c5ed6e858a8b49ea8e7fe6b575e72208b560
찬익 박 <piaocanyi1997@gmail.com>
2023-05-06 18:47:33 +0900

    add 2309.py

---
+++ 2309.py
@@ -0,0 +1,13 @@
+# combinations 사용
+# from itertools import combinations
+#
+# input_num = [int(input()) for _ in range(9)]
+#
+# combi_sort = []
+# for combi in combinations(input_num, 7):
+#     if sum(combi) == 100:
+#         combi_sort = sorted(combi)
+#
+#         for i in combi_sort:
+#             print(i)
+
