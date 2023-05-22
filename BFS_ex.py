f2b57a9c1832cdb390272143b5d22f346e4ca7c1
찬익 박 <piaocanyi1997@gmail.com>
2023-05-22 17:59:10 +0900

    add BFS_ex.py

---
+++ BFS_ex.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+adj = [[0] * 13 for _ in range(13)]
+adj[0][1] = adj[0][2] = 1
+adj[1][2] = adj[1][4] = 1
+
+# for row in adj:
+#     print(row)
+
+def bfs():
+    dq = deque()
+    dq.append(0)
+    while dq:
+        now = dq.popleft()
+        for nxt in range(13):
+            if adj[now][nxt]:
+                dq.append(nxt)
+
+
+bfs()
