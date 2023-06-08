c838ab70991c8f069223ec57cb25ccb22a45149a
찬익 박 <piaocanyi1997@gmail.com>
2023-06-08 09:19:23 +0900

    10051_Bottom-up.py

---
+++ 10051_Bottom-up.py
@@ -0,0 +1,18 @@
+MOD = 10007
+
+
+cache = [[0] * 1001 for _ in range(1001)]
+N, K = map(int, input().split())
+
+for i in range(1001):
+    cache[i][0] = cache[i][i] = 1
+    for j in range(1, i):
+        cache[i][j] = cache[i-1][j-1] + cache[i-1][j]
+        cache[i][j] %= MOD
+
+# for i in range(7):
+#     print(cache[i])
+# print()
+
+print(cache[N][K])
+
