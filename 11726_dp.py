9bf2e47eeca0dd838dc7e89968655f83acf79589
찬익 박 <piaocanyi1997@gmail.com>
2023-06-13 09:41:21 +0900

    modify 11726_dp.py

--- 11726_dp.py
+++ 11726_dp.py
@@ -1,4 +1,4 @@
-MOD = 10007
+MOD = 10_007
 
 dp = [0] * 1001
 dp[1] = 1
@@ -7,6 +7,6 @@ dp[2] = 2
 n = int(input())
 
 for i in range(3, 1001):
-    dp[i] = (dp[i - 1] + dp [i -2] % MOD)
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
 
 print(dp[n])
